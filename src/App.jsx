91970fbf8882d064261f83bfe6468d8999670d2d
SamuelAdom <samueldebrahmireku7@gmail.com>
2025-07-15 21:37:19 +0000

    ShopPage added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import ErrorPage from './Pages/ErrorPage'
+import ShopPage from './Pages/ShopPage'
 import MainLayout from './Layouts/MainLayout'
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route element={<MainLayout/>}>
         <Route path='/' element={<HomePage/>}></Route>
+        <Route path='/shop' element={<ShopPage/>}></Route>
         <Route path='*' element={<ErrorPage/>}></Route>
         </Route>
       </Routes>
