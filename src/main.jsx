d32cdcac554ca0303680ea845263a15a451934c5
SamuelAdom <samueldebrahmireku7@gmail.com>
2025-07-23 09:33:59 +0000

    clerk authentication added

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ShopContextProvider from './Context/ShopContext.jsx'
+ import { ClerkProvider } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
 
 createRoot(document.getElementById('root')).render(
+   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
   <BrowserRouter>
   <ShopContextProvider>
     <App />
     </ShopContextProvider>
   </BrowserRouter>,
+  </ClerkProvider>
 )
