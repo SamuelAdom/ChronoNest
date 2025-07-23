6153eee7c5431dad0d47045baf7139f32729ff34
SamuelAdom <samueldebrahmireku7@gmail.com>
2025-07-23 10:00:34 +0000

    Clerk authentication set up

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,9 +2,12 @@ import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FaShoppingCart } from "react-icons/fa";
 import { ShopContext } from "../Context/ShopContext";
+import { UserButton, useUser, useClerk } from "@clerk/clerk-react";
 
 export default () => {
   const [state, setState] = useState(false);
+const{openSignIn}=useClerk()
+const{user}=useUser()
   const{getTotal}=useContext(ShopContext)
 
   const navigation = [
@@ -104,24 +107,9 @@ className="text-gray-700 text-base lg:text-lg lg:hover:underline underline-offse
               {getTotal()}
               </div>
             </div>
-            <Link
-              to="javascript:void(0)"
-              className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"
-            >
-              Sign in
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="w-5 h-5"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </Link>
+     {user?<UserButton/>: <Link>
+      <button onClick={()=>openSignIn()} className="bg-gray-500 text-white text-lg px-8 py-2 rounded-md font-mono lg:hover:bg-gray-700 transition-all duration-500 cursor-pointer">Login</button>
+      </Link>}
           </div>
         </div>
       </div>
